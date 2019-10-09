c6658a6bec0a74067ae7de7081f05f6feb5351d6
Alexis_Huayna <ahuaynap@unsa.edu.pe>
2019-10-09 16:45:44 -0500

    navBar 15%

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { RouterModule, Routes } from "@angular/router";
+import { CursosComponent } from "./components/cursos/cursos.component";
+import { PublicacionesComponent } from "./components/publicaciones/publicaciones.component";
+import { LogInComponent } from "./components/log-in/log-in.component";
+import { RemoveWhiteSpacesPipe } from "./pipes/remove-white-spaces.pipe";
+
+const routes: Routes = [
+  {
+    path: "cursos",
+    component: CursosComponent
+  },
+  {
+    path: "publicaciones",
+    component: PublicacionesComponent
+  },
+  {
+    path: "login",
+    component: LogInComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CursosComponent,
+    PublicacionesComponent,
+    LogInComponent,
+    RemoveWhiteSpacesPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"]
+})
+export class NavBarComponent implements OnInit {
+  title = "Foro Universitario";
+  navigation_items = ["Cursos", "Publicaciones", "Log In"];
+  ruta_actual = "Cursos";
+
+  constructor() {}
+
+  ngOnInit() {}
+}
