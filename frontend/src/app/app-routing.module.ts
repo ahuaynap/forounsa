ce0f1c2d715c888703de44951bc0e7ddede0f24e
Alexis_Huayna <ahuaynap@unsa.edu.pe>
2019-10-09 20:59:10 -0500

    router modificado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CursosComponent } from "./components/cursos/cursos.component";
+import { PublicacionesComponent } from "./components/publicaciones/publicaciones.component";
+import { LogInComponent } from "./components/log-in/log-in.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cursos",
+    component: CursosComponent
+  },
+  {
+    path: "publicaciones",
+    component: PublicacionesComponent
+  },
+  {
+    path: "login",
+    component: LogInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,26 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { RouterModule, Routes } from "@angular/router";
 import { CursosComponent } from "./components/cursos/cursos.component";
 import { PublicacionesComponent } from "./components/publicaciones/publicaciones.component";
 import { LogInComponent } from "./components/log-in/log-in.component";
 import { RemoveWhiteSpacesPipe } from "./pipes/remove-white-spaces.pipe";
-
-const routes: Routes = [
-  {
-    path: "cursos",
-    component: CursosComponent
-  },
-  {
-    path: "publicaciones",
-    component: PublicacionesComponent
-  },
-  {
-    path: "login",
-    component: LogInComponent
-  }
-];
 
 @NgModule({
   declarations: [
@@ -34,7 +18,7 @@ const routes: Routes = [
     LogInComponent,
     RemoveWhiteSpacesPipe
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
