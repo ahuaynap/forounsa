f7bd0c1e7fe697bfd06e9739f18f2fa97c508312
kleber <kleber.sik@gmail.com>
2019-11-02 10:37:42 -0500

    p4

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,31 +1,30 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-nav-bar",
-  templateUrl: "./nav-bar.component.html",
-  styleUrls: ["./nav-bar.component.css"]
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  title = "Foro Universitario";
-  navigation_items = ["Log In"];
-
   constructor() {}
+  public title = 'Foro Universitario';
+  public navigationItems = ['Log In'];
 
   ngOnInit() {}
 
   updateNavBar(event) {
-    var ruta = event.target.name;
+    const ruta = event.target.name;
 
-    if (ruta == "login") {
-      this.navigation_items = [
-        "Inicio",
-        "Cursos",
-        "Publicaciones",
-        "Mis publicaciones",
-        "Logout"
+    if (ruta === 'login') {
+      this.navigationItems = [
+        'Inicio',
+        'Cursos',
+        'Publicaciones',
+        'Mis publicaciones',
+        'Logout'
       ];
-    } else if (ruta == "logout") {
-      this.navigation_items = ["Log In"];
+    } else if (ruta === 'logout') {
+      this.navigationItems = ['Log In'];
     }
   }
 }
