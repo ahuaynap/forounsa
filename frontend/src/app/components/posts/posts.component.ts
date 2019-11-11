1abdf6caad7d5a9eaa7962a59645c1f24219130b
kleber <kleber.sik@gmail.com>
2019-11-11 14:15:22 -0500

    k9

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { AuthService } from '../../services/auth.service';
+import { User } from 'src/app/interfaces/user.interface';
+import { Post } from 'src/app/interfaces/post.interface';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService, private authService: AuthService) { }
+
+  private currentUser: User = {
+    email: '',
+    name: ''
+  };
+
+  private posts: Post[];
 
   ngOnInit() {
+  }
+
+  getCurrentUser() {
+    this.authService.isAuth().subscribe( auth => {
+      if (auth) {
+        this.dataService.getUser(auth.email).subscribe(
+          res => {
+            this.currentUser = res;
+            this.getPosts(this.currentUser.id);
+          }
+        );
+      }
+    });
+  }
+
+  getPosts(currentUserId) {
+    this.dataService.getPostsUser(currentUserId).subscribe(
+      res => {
+        this.posts = res;
+      }
+    );
   }
 
 }
